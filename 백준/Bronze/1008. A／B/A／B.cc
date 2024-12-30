b2d58d985b1dd91ed8ee05a45063fcdf101c94e7
gud0217 <90571491+gud0217@users.noreply.github.com>
2024-12-30 15:28:51 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cout.precision(10);
+    
+    int a, b; cin >> a >> b;
+    cout << (double)a/b;
+
+    return 0;
+}
