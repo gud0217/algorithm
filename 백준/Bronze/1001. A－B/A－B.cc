3ea3fa24914fca80b45084cbf7f0adff97ba699c
gud0217 <90571491+gud0217@users.noreply.github.com>
2025-01-05 15:21:30 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.cc
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -1,11 +1,8 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int main(){
-
-    int a, b;
-    cin >> a >> b;
-    cout << a - b;
-
-    return 0;
+    int a,b; cin >> a >> b;
+    
+    cout << a-b;
 }
