064fa8ad13db355c19b496f5e2291939bd552562
박형진 <gud5223@gmail.com>
2025-10-14 11:40:30 +0900

    [level 0] Title: 문자열 밀기, Time: 13.61 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(String A, String B) {
+        int answer = 0;
+        int length = A.length();
+
+        for(int i = 0; i < length; i++) {
+            if(A.equals(B)) {
+                break;
+            }
+
+            A = A.charAt(length - 1) + A.substring(0, length - 1);
+            System.out.println(A);
+            answer++;
+        }
+
+        if(answer == length) {
+            answer = -1;
+        }
+
+        return answer;
+    }
+}
