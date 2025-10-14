dc50b0e395d54b796149ab6a2db5cb59e4d5bd7a
박형진 <gud5223@gmail.com>
2025-10-14 09:57:33 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,18 +1,6 @@
 class Solution {
     public int solution(int chicken) {
-        int answer = 0; // 시켜먹은 서비스 치킨 수
-        int service = 0; // 쿠폰으로 시킨 치킨으로 받은 쿠폰
-        int nam = 0; // 남은 쿠폰 수
-        
-        while(chicken > 9) {
-            service = chicken / 10;
-            nam = chicken % 10;
-            answer += service;
-            chicken = service + nam;
-        }
-        
+        int answer = -1;
         return answer;
     }
-    
-
 }
