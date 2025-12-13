f3e9555dfa27c82a99ed8ba6abbfa1899a5f1b32
박형진 <gud5223@gmail.com>
2025-12-13 13:00:20 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 78.32 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,10 +1,11 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
         HashMap<String, Integer> hm = new HashMap<>();
+        
         for (String player : participant) {
             hm.put(player, hm.getOrDefault(player, 0) + 1);
         }
@@ -13,10 +14,9 @@ class Solution {
             hm.put(player, hm.get(player) - 1);
         }
         
-        for (String key : hm.keySet()) {
-            if(hm.get(key) != 0) {
-                answer = key;
-                break;
+        for (Map.Entry<String, Integer> entry : hm.entrySet()) {
+            if (entry.getValue() == 1) {
+                answer = entry.getKey();
             }
         }
         
