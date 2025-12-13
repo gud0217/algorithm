0263df602ad51a20c64a7a3beeefad8a4cb34aba
박형진 <gud5223@gmail.com>
2025-12-13 16:32:58 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,23 +1,25 @@
-// 1. clothes 배열을 순회하며, 의상의 종류별 개수를 구한다
-// 2. 답 = (A종류 수 + 1) * (B종류 수 + 1) .. - 1
-import java.util.HashMap;
+// HashMap<종류,개수>
+// (종류별 개수 + 1) * ... ( )
+
+import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-
+        
         HashMap<String,Integer> hm = new HashMap<>();
-
+        
+        // 해시맵에 종류 별 개수 저장
         for (String[] str : clothes) {
             hm.put(str[1], hm.getOrDefault(str[1], 0) + 1);
         }
-
-        for (String str : hm.keySet()) {
-            answer *= (hm.get(str) + 1);
+        
+        // 종류 별 개수 곱하기
+        for (String s : hm.keySet()) {
+            answer *= (hm.get(s) + 1);
         }
         
-        answer--;
-
-        return answer;
+        // 다 안입는 경우 제외
+        return answer - 1;
     }
 }
