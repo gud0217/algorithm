2d68dd5a70a187911a1f79225f6615a6edfa6df6
박형진 <gud5223@gmail.com>
2025-12-13 15:42:06 +0900

    [level 1] Title: 폰켓몬, Time: 4.54 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,27 +1,22 @@
-// 1. nums 길이 구하기
-// 2. nums 배열 중복 제거한 길이 구하기
-import java.util.HashSet;
+// HashSet을 이용, 포켓몬 종류 압축
+// size >= n/2면 n/2, size < n/2면 size
+import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
         
-        // 폰켓몬 수
-        int numbers = nums.length;
-        int max = numbers / 2;
-        
-        // 중복 제거
         HashSet<Integer> hs = new HashSet<>();
-        
-        for (int i : nums) {
-            hs.add(i);
+        for (int i = 0; i < nums.length; i++) {
+            hs.add(nums[i]);
         }
         
-        // 총 종류가, max보다 크다면 max이고, 적다면 종류수만큼이다.
-        if (hs.size() > max) {
-            answer = max;
+        int size = hs.size();
+        
+        if (size >= (nums.length / 2)) {
+            answer = nums.length / 2;
         } else {
-            answer = hs.size();
+            answer = size;
         }
         
         return answer;
