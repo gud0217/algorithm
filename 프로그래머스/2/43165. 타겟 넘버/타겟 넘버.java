0886551807ecd75f3c903e7fba75a5f5ce588291
박형진 <gud5223@gmail.com>
2025-12-14 01:01:47 +0900

    [level 2] Title: 타겟 넘버, Time: 5.22 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,35 +1,25 @@
 import java.util.*;
 
 class Solution {
+    int answer = 0;
+    
     public int solution(int[] numbers, int target) {
-        
-        // 현재까지 만들어진 합들을 저장할 큐
-        Queue<Integer> queue = new ArrayDeque<>();
-        
-        // 시작 상태
-        queue.offer(0);
-        
-        // numbers 배열의 숫자를 하나씩 사용
-        for (int num : numbers) {
-            
-            // 현재 큐에 들어있는 원소 개수, 이 개수만큼만 꺼내서 적용해야 함
-            int size = queue.size();
-            
-            // 같은 레벨
-            for (int i = 0; i < size; i++) {
-                int cur = queue.poll(); // 이전 단계에서 만든 합
-                queue.offer(cur + num); // 현재 숫자 더하는 경우
-                queue.offer(cur - num); // 현재 숫자 빼는 경우
-            }
-        }
-        
-        // 모든 숫자 다 사용 후, target과 같은 합의 개수를 센다
-        int answer = 0;
-        while (!queue.isEmpty()) {
-            if (queue.poll() == target) {
-                answer ++;
-            }
-        }
+        dfs(0, 0, numbers, target);
         return answer;
+    }
+    
+    // dfs
+    public void dfs(int idx, int sum, int[] numbers, int target) {
+        // 1. 종료 조건
+        if (idx == numbers.length) {
+            if (sum == target) {
+                answer++;
+            }
+            return;
+        }
+    // 2. 현재 숫자를 더하는 경우
+    dfs(idx + 1, sum + numbers[idx], numbers, target);
+    // 3. 현재 숫자를 빼는 경우
+    dfs(idx + 1, sum - numbers[idx], numbers, target);
     }
 }
