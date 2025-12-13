be9170e37accb1fd2ec225bc7c0c5a7dae38f68c
박형진 <gud5223@gmail.com>
2025-12-13 15:57:17 +0900

    [level 2] Title: 전화번호 목록, Time: 345.32 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,20 +1,26 @@
-// 1. 문자열 배열을, 문자열 길이 순으로 정렬
-// 2. 경우의 수 전부 계산
-import java.util.Arrays;
+// phone_book을 사전순으로 정렬
+// 뒤에있는걸 앞에만큼 짤라서 비교?
+import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         
+        // 사전순 정렬
         Arrays.sort(phone_book);
-                
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].indexOf(phone_book[i]) == 0) {
-                answer = false;
-                break;
+        
+        for(int i = 0; i < phone_book.length - 1; i++) {
+            int length1 = phone_book[i].length();
+            int length2 = phone_book[i + 1].length();
+            
+            if (length1 < length2) {
+                String str = phone_book[i + 1].substring(0, length1);
+                if (phone_book[i].equals(str)) {
+                    answer = false;
+                    break;
+                }
             }
         }
-        
         return answer;
     }
 }
