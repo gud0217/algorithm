0789c5d1ec2cc729c2d27b6330fee193085066af
박형진 <gud5223@gmail.com>
2025-12-14 12:19:42 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 87.8 MB, 시간: 0.04 ms
+메모리: 76 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 07일 18:00:38
+2025년 12월 14일 12:19:40
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,51 +1,48 @@
-// 각각 며칠후에 완성되는지 계산
-//  [7, 3, 9], [5, 10, 1, 1, 20, 1]
-
-// 인접 비교로, 앞이 더 크면 뒤를 앞이랑 맞춰버림
-//  [7, 7, 9], [5, 10, 10, 10, 20, 20]
-
-// 중복된 수 만큼 리턴
-//  첫 원소를 잡고, 같으면 count++;
-//  다르면 pre초기화, count초기화;
-
-import java.util.ArrayList;
+// 남은 작업량 계산
+// 남은 작업 일 계산
+// 뒷 작업이 자기보다 클 때 까지 조건 확인
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        
-        // 각 작업에 필요한 날짜 배열
-        int[] days = new int[progresses.length];
+        // 남은 작업량 계산
         for (int i = 0; i < progresses.length; i++) {
-            days[i] = (100 - progresses[i]) / speeds[i];
-            if ((100 - progresses[i]) % speeds[i] != 0) days[i]++;
+            progresses[i] = 100 - progresses[i];
         }
         
-        // 앞선 작업이 더 오래걸리면, 뒷 작업도 맞춤
-        for (int i = 0; i < progresses.length - 1; i++) {
-            if (days[i] > days[i + 1]) {
-                days[i + 1] = days[i];
+        // 남은 일 수 계산
+        for (int i = 0; i < progresses.length; i++) {
+            int remain = progresses[i] / speeds[i];
+            // 나누어 떨어지지 않는다면 +1일
+            if (progresses[i] % speeds[i] > 0) {
+                remain++;
             }
+            progresses[i] = remain;
         }
         
-        // 몇번 반복되는지 구하기
-        int pre = days[0];
+        List<Integer> list = new ArrayList<>();
+        
+        int base = progresses[0];
         int count = 1;
-        ArrayList<Integer> list = new ArrayList<>();
         
         for (int i = 1; i < progresses.length; i++) {
-            if (pre == days[i]) {
+            // 같이 배포 가능
+            if (base >= progresses[i]) {
                 count++;
-            } else {
-                list.add(count);
-                pre = days[i];
-                count = 1;
             }
-            if (i + 1 == progresses.length) {
+            
+            // 새 배포
+            else {
                 list.add(count);
+                base = progresses[i];
+                count = 1;
             }
         }
         
-        int answer[] = new int[list.size()];
+        // 마지막 묶음 추가
+        list.add(count);
+        
+        int[] answer = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i);
         }
