f3e338b35fd131f1921be4a3eb464d8c84aca07e
박형진 <gud5223@gmail.com>
2025-12-24 10:58:13 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.07 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -9,10 +9,12 @@
 
 class Solution {
     public boolean solution(String s) {
-        boolean answer = false;
+        if (s.length() != 4 && s.length() != 6) return false;
         
-        if (s.matches("[0-9]{4}") || s.matches("[0-9]{6}")) answer = true;
+        for (char c : s.toCharArray()) {
+            if (!Character.isDigit(c)) return false;
+        }
         
-        return answer;
+        return true;
     }
 }
