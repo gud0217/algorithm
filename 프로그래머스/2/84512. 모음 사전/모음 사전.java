7b2f61b19c8167dca379d0b80d232aa85c0051d0
박형진 <gud5223@gmail.com>
2026-01-14 15:16:27 +0900

    [level 2] Title: 모음 사전, Time: 26.53 ms, Memory: 96.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    private static final char[] CHARS = "AEIOU".toCharArray();
+    
+    public int solution(String word) {
+        List<String> words = new ArrayList<>();
+        generate("", words);
+        return words.indexOf(word);
+    }
+    
+    private void generate(String word, List<String> words) {
+        words.add(word);
+        
+        if (word.length() == 5) return;
+        for (char c : CHARS) {
+            generate(word + c, words);
+        }
+    }
+}
