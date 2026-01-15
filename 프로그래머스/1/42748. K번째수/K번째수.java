da4b68dc5900e74be636b3ce005381ddf74bb2eb
박형진 <gud5223@gmail.com>
2026-01-15 15:44:12 +0900

    [level 1] Title: K번째수, Time: 0.48 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,24 +1,25 @@
-// commands 길이만큼 반복
-//  i번째 숫자부터, j번째포함 자르기
-//  정렬하기
-//  k번째 숫자 찾기
-//  결과값에 넣기
-
+// i번째부터, j번째까지 자르기
+// 정렬하기
+// k번째 수 구하기
 import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        List<Integer> list = new ArrayList<>();
+        int[] answer = new int[commands.length]; // 답, 길이는 commands의 배열 길이만큼.
         
-        for (int[] i : commands) {
-            int[] slice = Arrays.copyOfRange(array, i[0] - 1, i[1]);
-            Arrays.sort(slice);
-            list.add(slice[i[2] - 1]);
-        }
-        
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
+        // 그만큼 반복
+        for (int i = 0; i < commands.length; i++) {
+            int size = commands[i][1] - commands[i][0] + 1; // 자른 길이
+            int[] arr = new int[size]; // 만들기
+            
+            // 복사하기
+            for (int j = 0; j < size; j++) {
+                arr[j] = array[commands[i][0] - 1 + j]; // 복사
+            }
+            
+            Arrays.sort(arr);
+            
+            answer[i] = arr[commands[i][2] - 1];
         }
         
         return answer;
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 87.4 MB, 시간: 1.14 ms
+메모리: 86.4 MB, 시간: 0.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 14일 00:43:50
+2026년 01월 15일 15:44:10
 
 ### 문제 설명
 
